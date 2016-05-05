3d4fa8f90c94c02a3f8eef3378f8f70e6d64df38
quazardous <berliozdavid@gmail.com>
2016-05-05 19:26:22 +0200

    named form are in silex now

--- src/Acme/Application.php
+++ src/Acme/Application.php
@@ -5,11 +5,9 @@ use Quazardous\Silex\PackableApplication;
 use Silex\Application\TwigTrait;
 use Silex\Application\UrlGeneratorTrait;
 use Silex\Application\FormTrait;
-use Quazardous\Silex\Application\NamedFormTrait;
 
 class Application extends PackableApplication {
     use TwigTrait;
     use UrlGeneratorTrait;
     use FormTrait;
-    use NamedFormTrait;
 }
